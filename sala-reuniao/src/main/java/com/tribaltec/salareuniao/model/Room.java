77e844471824ce0b7c4c5e21b474402aeb7a41e9
mayquiler <mayconrodriguesribeiro@gmail.com>
2021-09-09 20:57:33 -0300

    Sistema Api Rest Java e Angular 1 commit

---
+++ sala-reuniao/src/main/java/com/tribaltec/salareuniao/model/Room.java
@@ -0,0 +1,102 @@
+package com.tribaltec.salareuniao.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.util.Objects;
+
+@Entity
+@Table(name="meetingroom")
+public class Room {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    @Column(name = "name", nullable = false)
+    private String name;
+    @Column(name = "date", nullable = false)
+    private String date;
+    @Column(name = "startHour", nullable = false)
+    private String startHour;
+    @Column(name = "endHour", nullable = false)
+    private String endHour;
+
+    public Room(){
+
+    }
+
+    public Room(long id, String name, String date, String startHour, String endHour) {
+        this.id = id;
+        this.name = name;
+        this.date = date;
+        this.startHour = startHour;
+        this.endHour = endHour;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getStartHour() {
+        return startHour;
+    }
+
+    public void setStartHour(String startHour) {
+        this.startHour = startHour;
+    }
+
+    public String getEndHour() {
+        return endHour;
+    }
+
+    public void setEndHour(String endHour) {
+        this.endHour = endHour;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Room room = (Room) o;
+        return id == room.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Room{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", date='" + date + '\'' +
+                ", startHour='" + startHour + '\'' +
+                ", endHour='" + endHour + '\'' +
+                '}';
+    }
+}
